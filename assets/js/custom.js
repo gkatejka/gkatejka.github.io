cff58b52c8337aa2d111b237174909c13861aa5a
gkatejka <kate.golovkina@gmail.com>
2016-11-29 03:16:51 +0300

    added js for check essey page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,85 +6,116 @@ $(function(){
     $('#bookprogress>.book-progress').detach().prependTo('#booknav');
   }
 
-  // // ESSEY
-  // var esseyItem = document.getElementById('quality').getElementsByTagName('input');
-  // var pointsEssey = 0;
-  // for (var j = 0; j < esseyItem.length; j++) {
-  //   if( esseyItem[j].checked) {
-  //     pointsEssey = + esseyItem[j].value;
-  //     break;
-  //   }
-  // }
+  function checkAbstract(){
+    var pointsMin = 0;
+    var pointsMax = 100;      // Максимум баллов
+    var thesisCounter;        // Кол-во тезисов
+    var pointsQuality = 0;    // Баллы за качество
 
-  // document.getElementById('pointsAll').innerHTML = pointsEssey;
-  // document.getElementById('pointsAbstract').value = pointsEssey;
+    // Находим все тезисы
+    var thesesItem = document.getElementById('thesesInputs').getElementsByTagName('input');
 
-  
-  // POINTS
-  var pointsMin = 0;
-  var pointsMax = 100;      // Максимум баллов
-  var thesisCounter;        // Кол-во тезисов
-  var pointsQuality = 0;    // Баллы за качество
+    thesisCounter = thesesItem.length;
+    var thesisValue = pointsMax / thesisCounter;    // Кол-во баллов за 1 тезис
 
-  // Находим все тезисы
-  var thesesItem = document.getElementById('thesesInputs').getElementsByTagName('input');
+    function Culc() {
+      var thesisChecked = 0;    // Кол-во отмеченных тезисов
+      var pointsTotal = 0;      // Сумма баллов
 
-  thesisCounter = thesesItem.length;
-  var thesisValue = pointsMax / thesisCounter;    // Кол-во баллов за 1 тезис
+      // Считаем кол-во отмеченных тезисов
+      for (var l = 0; l < thesisCounter; l++) {
+        if(thesesItem[l].checked) {
+          thesisChecked++;
+        }
+      }
 
-  function Culc() {
-    var thesisChecked = 0;    // Кол-во отмеченных тезисов
-    var pointsTotal = 0;      // Сумма баллов
+      // Считаем быллы за качество конспекта
+      var qualityElem = document.getElementById('quality').getElementsByTagName('input');
 
-    // Считаем кол-во отмеченных тезисов
-    for (var l = 0; l < thesisCounter; l++) {
-      if(thesesItem[l].checked) {
-        thesisChecked++;
+      for (var j = 0; j < qualityElem.length; j++) {
+        if( qualityElem[j].checked) {
+          pointsQuality = + qualityElem[j].value;
+          break;
+        }
+      }
+
+      // Суммируем баллы за тезисы и качество
+      pointsTotal = (thesisValue*thesisChecked) + pointsQuality;
+
+      if ( pointsTotal >= pointsMax ) {
+        pointsTotal = pointsMax;
+        return pointsTotal;
+      } else if ( pointsTotal < pointsMin ) {
+        pointsTotal = pointsMin;
+        return pointsTotal;
+      } else {
+        return pointsTotal;
       }
     }
 
-    // Считаем быллы за качество конспекта
-    var qualityElem = document.getElementById('quality').getElementsByTagName('input');
+    // Подсчет суммы баллов
+    function totalPoints(){
+      var total = Culc();
+      document.getElementById('pointsAll').innerHTML = total;
+      document.getElementById('pointsAbstract').value = total;
+      return total;
+    }
 
-    for (var j = 0; j < qualityElem.length; j++) {
-      if( qualityElem[j].checked) {
-        pointsQuality = + qualityElem[j].value;
-        break;
+    var inputs = document.getElementById('checkIdeas');
+    var input = inputs.getElementsByTagName('input');
+
+    for (var i = 0; i < input.length; i++) {
+      input[i].addEventListener('click', set_handler(i));
+    }
+
+    function set_handler(i){
+      return function () {
+        totalPoints();
+      };
+    }
+  }
+
+  if (document.getElementById('thesesInputs')){
+    checkAbstract();
+  }
+
+
+  function checkEssey(){
+    var esseyItems = document.getElementById('esseyInputs').getElementsByTagName('input');
+
+    function culcPointsEssey() {
+      var pointsEssey = 0;
+      for (var j = 0; j < esseyItems.length; j++) {
+        if( esseyItems[j].checked) {
+          pointsEssey = + esseyItems[j].value;
+          break;
+        }
       }
+      return pointsEssey;
+    }
+    
+    function totalPointsEssey(){
+      var total = culcPointsEssey();
+      document.getElementById('pointsAllEssey').innerHTML = total;
+      document.getElementById('pointsEssey').value = total;
+      return total;
     }
 
-    // Суммируем баллы за тезисы и качество
-    pointsTotal = (thesisValue*thesisChecked) + pointsQuality;
+    for (var i = 0; i < esseyItems.length; i++) {
+      esseyItems[i].addEventListener('click', set_handler(i));
+    }
 
-    if ( pointsTotal >= pointsMax ) {
-      pointsTotal = pointsMax;
-      return pointsTotal;
-    } else if ( pointsTotal < pointsMin ) {
-      pointsTotal = pointsMin;
-      return pointsTotal;
-    } else {
-      return pointsTotal;
+    function set_handler(i){
+      return function () {
+        totalPointsEssey();
+      };
     }
   }
 
-  // Подсчет суммы баллов
-  function totalPoints(){
-    var total = Culc();
-    document.getElementById('pointsAll').innerHTML = total;
-    document.getElementById('pointsAbstract').value = total;
-    return total;
+  if (document.getElementById('esseyInputs')){
+    checkEssey();
   }
 
-  var inputs = document.getElementById('checkIdeas');
-  var input = inputs.getElementsByTagName('input');
 
-  for (var i = 0; i < input.length; i++) {
-    input[i].addEventListener('click', set_handler(i));
-  }
 
-  function set_handler(i){
-    return function () {
-      totalPoints();
-    };
-  }
 });
